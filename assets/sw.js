f41ba1d75fd525a7f63a6a0c0b69aafc90a0c301
An Tran <antranprm@gmail.com>
2026-03-06 09:07:18 -0800

    Prefer fresh network fetch over cached fetch for now

--- assets/sw.js
+++ assets/sw.js
@@ -19,7 +19,7 @@ self.addEventListener('install', function (e) {
 self.addEventListener('fetch', function (e) {
   e.respondWith(
     caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
+      return fetch(e.request) || response;
     })
   );
 });
